348ff468406bc9d7683d4937d72ec3e565d36cea
gazt88 <gazt88@users.noreply.github.com>
2025-07-14 14:08:05 +0900

    supabase 인스턴스 중복 제거 및 utils/supabase.js 단일화

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,8 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+import { supabase } from '../utils/supabase';
 
 // 이벤트 CRUD
 export async function getEvents() {
